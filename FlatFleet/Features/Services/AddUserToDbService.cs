2f9c07f33d49a88ef93fac2489bef3881a11f310
X3ros7 <a.a.prosv2005@gmail.com>
2024-06-20 10:50:38 +0300

    Fix AddUserToDbService

--- FlatFleet/Features/Services/AddUserToDbService.cs
+++ FlatFleet/Features/Services/AddUserToDbService.cs
@@ -4,29 +4,34 @@ namespace FlatFleet.Features.Services
 {
     public class AddUserToDbService
     {
-        private SignUpPageViewModel _viewModel;
         private FirestoreDb _db;
         private CurrentUserStore _userStore;
 
-        public AddUserToDbService(SignUpPageViewModel viewModel, FirestoreDb db, CurrentUserStore userStore)
+        public AddUserToDbService( FirestoreDb db, CurrentUserStore userStore)
         {
-            _viewModel = viewModel;
             _db = db;
             _userStore = userStore;
         }
 
-        public async Task SaveUserToDb()
+        public async Task SaveUserToDb(string fullName, string email, string phoneNumber)
         {
-            await _db.Collection("users")
-                        .Document(_userStore.CurrentUser.Uid)
-                        .SetAsync(new
-                        {
-                            fullName = _viewModel.FullName,
-                            email = _viewModel.Email,
-                            phoneNumber = _viewModel.PhoneNumber,
-                            accountType = "undefined",
-                            documents = Array.Empty<string>()
-                        });
+            try
+            {
+                await _db.Collection("users")
+                            .Document(_userStore.CurrentUser.Uid)
+                            .SetAsync(new
+                            {
+                                fullName = fullName,
+                                email = email,
+                                phoneNumber = phoneNumber,
+                                accountType = "undefined",
+                                documents = Array.Empty<string>()
+                            });
+            }
+            catch (Exception ex) 
+            {
+                await Application.Current.MainPage.DisplayAlert("Error", $"Cannot add user to database: {ex.Message}", "Ok");
+            }
         }
     }
 }
