33d7a23801689a27dbf0cdc2ffe8f4bba7e5e294
Arthur Shcherban <kruasain@gmail.com>
2024-06-21 12:43:23 +0300

    The sign-up page has pop-ups as well.

--- FlatFleet/Features/SignUp/SignUpCommand.cs
+++ FlatFleet/Features/SignUp/SignUpCommand.cs
@@ -1,6 +1,9 @@
-﻿using Firebase.Auth;
+﻿using CommunityToolkit.Maui.Views;
+using Firebase.Auth;
 using FlatFleet.Features.Services;
 using FlatFleet.Models.Users;
+using FlatFleet.Views;
+using System.Diagnostics;
 
 namespace FlatFleet.Features.SignUp
 {
@@ -21,6 +24,12 @@ namespace FlatFleet.Features.SignUp
 
         protected override async Task ExecuteAsync(object parameter)
         {
+            var loadingPopup = new LoadingScreenPopUp();
+
+            Application.Current.MainPage.ShowPopupAsync(loadingPopup);
+
+            Debug.WriteLine("Popup was opened");
+
             try
             {
                 var userCredential = await _authClient.CreateUserWithEmailAndPasswordAsync(_viewModel.Email, _viewModel.Password, _viewModel.FullName);
@@ -31,12 +40,25 @@ namespace FlatFleet.Features.SignUp
                     email: _viewModel.Email,
                     phoneNumber: _viewModel.PhoneNumber
                     );
+
+                await loadingPopup.CloseAsync();
                     
                 await Application.Current.MainPage.DisplayAlert("Success", "Successfully signed up!", "Ok");
+
+                var welcomePopup = new WelcomePopUp();
+
+                Application.Current.MainPage.ShowPopupAsync(welcomePopup);
+
+                await Task.Run(() => Thread.Sleep(2000));
+
+                await welcomePopup.CloseAsync();
+
                 await Shell.Current.GoToAsync("//SelectAccountType");
             }
             catch (Exception)
             {
+                await loadingPopup.CloseAsync();
+
                 await Application.Current.MainPage.DisplayAlert("Error", "Failed to sign up. Please try again later.", "Ok");
             }
         }
