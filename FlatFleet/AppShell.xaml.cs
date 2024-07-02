998765af9a653a8db9fd620a157bf271f3ebca70
aapro <aapro@ALEX_LAPTOP>
2024-07-02 12:08:38 +0300

    Add navigation from all account type pages to SelectAccountPage

--- FlatFleet/AppShell.xaml.cs
+++ FlatFleet/AppShell.xaml.cs
@@ -6,11 +6,23 @@ namespace FlatFleet
     {
         public AppShell()
         {
+            /*
+              case "//SelectManagementCompany":
+                case "//SelectDoubt":
+                case "//SelectTenantOfHouse":
+                case "//SelectHouseCommittee"
+             */
+
+
             InitializeComponent();
             Routing.RegisterRoute("GetStarted", typeof(GetStartedPage));
             Routing.RegisterRoute("SignUp", typeof(SignUpPage));
             Routing.RegisterRoute("TermsOfService", typeof(TermsOfServicePage));
             Routing.RegisterRoute("PrivacyPolicy", typeof(PrivacyPolicyPage));
+            Routing.RegisterRoute("SelectDoubt", typeof(DoubtPage));
+            Routing.RegisterRoute("SelectManagementCompany", typeof(ManagementCompanyPage));
+            Routing.RegisterRoute("SelectTenantOfHouse", typeof(TenantOfHousePage));
+            Routing.RegisterRoute("SelectHouseCommittee", typeof(HouseCommitteePage));
         }
     }
 }
