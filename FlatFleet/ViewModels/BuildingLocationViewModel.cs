ee4377c0793f3f79814454169b103b6c04d19625
X3ros7 <a.a.prosv2005@gmail.com>
2024-07-06 20:52:47 +0300

    Add BuidlingLocationViewModel

---
+++ FlatFleet/ViewModels/BuildingLocationViewModel.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+
+namespace FlatFleet.ViewModels
+{
+    public class BuildingLocationViewModel
+    {
+		private string _homeLocation;
+
+		public string HomeLocation
+		{
+			get { return _homeLocation; }
+			set { _homeLocation = value; }
+		}
+
+		private ICommand SubmitCommand { get; }
+        
+        public BuildingLocationViewModel()
+        {
+            SubmitCommand = new Command(Submit);
+        }
+
+        private async void Submit()
+        {
+            // TODO: Add route to next page 
+            //await Shell.Current.GoToAsync("");
+        }
+    }
+}
