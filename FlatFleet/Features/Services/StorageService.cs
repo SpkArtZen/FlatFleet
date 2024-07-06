379bcf65c5264b98a42a3c9a0f070425cc7826cd
X3ros7 <a.a.prosv2005@gmail.com>
2024-07-06 15:19:27 +0300

    Add StorageService.cs

---
+++ FlatFleet/Features/Services/StorageService.cs
@@ -0,0 +1,38 @@
+﻿using Firebase.Storage;
+using FlatFleet.Models.Users;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FlatFleet.Features.Services
+{
+    public class StorageService
+    {
+        private readonly FirebaseStorage _storage;
+        private readonly CurrentUserStore _userStore;
+
+        public StorageService(FirebaseStorage storage, CurrentUserStore userStore) 
+        { 
+            _storage = storage;
+            _userStore = userStore;
+        }
+
+        public async Task LoadFilesToStorage(IEnumerable<FileResult> files)
+        {
+            foreach (var file in files)
+            {
+                using (var stream = await file.OpenReadAsync())
+                {
+                    await _storage
+                        .Child($"documents/{_userStore.CurrentUser.Uid}/{file.FileName}")
+                        .PutAsync(stream);
+
+                    await Application.Current.MainPage.DisplayAlert("Success", "Files were successfully loaded!", "Ok");
+                }
+                await Shell.Current.GoToAsync("..");
+            }
+        }
+    }
+}
