e142cc507fe24c699efef8196f2220ec1267f888
X3ros7 <a.a.prosv2005@gmail.com>
2024-07-06 15:59:49 +0300

    Add some user info on AccountDashboardPage

--- FlatFleet/ViewModels/AccountDashboardPageViewModel.cs
+++ FlatFleet/ViewModels/AccountDashboardPageViewModel.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using FlatFleet.Features.Services;
+using FlatFleet.Models.Users;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -8,6 +10,19 @@ namespace FlatFleet.ViewModels
 {
     public class AccountDashboardPageViewModel
     {
+        private readonly CurrentUserStore _userStore;
+        private readonly DbService _db;
+
+        public AccountDashboardPageViewModel(CurrentUserStore userStore, DbService db)
+        {
+            _userStore = userStore;
+            _db = db;
+        }
+
+        public string UserName => _userStore.CurrentUser.Info.DisplayName;
+        public string UserEmail => _userStore.CurrentUser.Info.Email;
+        public string UserPhone => null;
+        public string UserAccountType => null;
 
     }
 }
