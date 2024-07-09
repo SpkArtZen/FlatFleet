901b12697cd296d8b51dad641b84573d61f94ece
X3ros7 <a.a.prosv2005@gmail.com>
2024-07-09 15:43:48 +0300

    Add color change if frame is empty in BuildingDefinitionPage

--- FlatFleet/Pages/BuildingDefinitionsPage.xaml.cs
+++ FlatFleet/Pages/BuildingDefinitionsPage.xaml.cs
@@ -1,9 +1,12 @@
+using FlatFleet.ViewModels;
+
 namespace FlatFleet.Pages;
 
 public partial class BuildingDefinitionsPage : ContentPage
 {
-	public BuildingDefinitionsPage()
+	public BuildingDefinitionsPage(BuildingDefinitionsViewModel viewModel)
 	{
 		InitializeComponent();
+		BindingContext = viewModel;
 	}
 }
