9fe8a7d6a542ce1298f555fd95d6627eaca78fba
X3ros7 <a.a.prosv2005@gmail.com>
2024-07-15 13:52:59 +0300

    Add combobox and two frames to FloorDefinitionPage

--- FlatFleet/ViewModels/FloorDefinitionViewModel.cs
+++ FlatFleet/ViewModels/FloorDefinitionViewModel.cs
@@ -1,7 +1,74 @@
-﻿namespace FlatFleet.Pages
-{
-    public class FloorDefinitionViewModel
-    {
+﻿using MvvmHelpers;
+using FlatFleet.Features.Navigation;
+using FlatFleet.Pages;
+using System.Windows.Input;
+using FlatFleet.Features.Services;
 
+namespace FlatFleet.Pages
+{
+    public class FloorDefinitionViewModel : BaseViewModel
+    {
+        private string _selectedText = "First floor";
+        public string SelectedText
+        {
+            get { return _selectedText; }
+            set
+            {
+                SetProperty(ref _selectedText, value);
+            }
+        }
+
+        private bool _isOpened;
+        public bool IsOpened
+        {
+            get { return _isOpened; }
+            set
+            {
+                SetProperty(ref _isOpened, value);
+            }
+        }
+
+        private List<string> _floors;
+        public List<string> Floors
+        {
+            get { return _floors; }
+            set
+            {
+                SetProperty(ref _floors, value);
+            }
+        }
+
+        private string _isSelected = "";
+        public string IsSelected
+        {
+            get { return _isSelected; }
+            set
+            {
+                SetProperty(ref _isSelected, value);
+            }
+        }
+
+        public ICommand SelectedItem { get; }
+
+        public FloorDefinitionViewModel()
+        {
+            Floors = new()
+            {
+                "First floor",
+                "Second floor",
+                "Third floor",
+                "Fourth floor"
+            };
+
+            SelectedItem = new Command((obj) => SelectedItemAction(obj));
+        }
+
+        private void SelectedItemAction(object obj)
+        {
+            IsOpened = false;
+            SelectedText = obj.ToString();
+
+
+        }
     }
 }
